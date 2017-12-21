65a9b349acb7cae86d4fd9d91a450659190a001e
Santosh Yadav <yadav7866@gmail.com>
2017-12-21 14:22:02 +0530

    Comment section updated
    All the comment section updated for better understanding

--- _2.ino/_2.ino.ino
+++ _2.ino/_2.ino.ino
@@ -1,11 +1,12 @@
+//Global input declearation section there are two input ports five out put port 
 int ir2=13,ip1=12,ip2=11,op1=3,op2=4,op3=5,op4=6,val1,val2;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ir2,OUTPUT);
-  pinMode(ip1,INPUT);
-  pinMode(ip2,INPUT);
-  pinMode(op1,OUTPUT);
-  pinMode(op2,OUTPUT);
+  pinMode(ir2,OUTPUT);    //for 5v supply to the one of the ir sensor 
+  pinMode(ip1,INPUT);     //Input from first ir sensor
+  pinMode(ip2,INPUT);     //Input from second ir sensor 
+  pinMode(op1,OUTPUT);    //
+  pinMode(op2,OUTPUT);    // setup for output pins 3,4,5,6
   pinMode(op3,OUTPUT);
   pinMode(op4,OUTPUT);
   
@@ -13,14 +14,15 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(ir2,HIGH);
-  val1=digitalRead(ip1);
+  //main function starts from here
+  digitalWrite(ir2,HIGH); // Providing 5v power supply to the second ir sensor
+  val1=digitalRead(ip1);  //Reading data from ir sensors
   val2=digitalRead(ip2);
-  if(val1==HIGH&&val2==HIGH)
+  if(val1==HIGH&&val2==HIGH)  //Condition for forward motion
   {
-    digitalWrite(op1,HIGH);
+    digitalWrite(op1,HIGH); // for first motor
     digitalWrite(op2,LOW);
-    digitalWrite(op3,HIGH);
+    digitalWrite(op3,HIGH);//for second motor
     digitalWrite(op4,LOW);
   }
   else 
