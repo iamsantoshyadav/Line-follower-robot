47dc48985e74c2d938782f41d87076405f73b1e3
Santosh Yadav <yadav7866@gmail.com>
2017-12-21 10:54:59 +0530

    dummy
    dummy

--- _2.ino/_2.ino.ino
+++ _2.ino/_2.ino.ino
@@ -1,4 +1,4 @@
-int ir2=13,ip1=12,ip2=11,op1=3,op2=4,op3=5,op4=6,val1,val2;
+int ir2=13,ip1=12,ip2=11,op1=3,op2=4,op3=5,op4=6,val1,val2,value3;
 void setup() {
   // put your setup code here, to run once:
   pinMode(ir2,OUTPUT);
